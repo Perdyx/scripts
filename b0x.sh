9601db2af8aba992c632363cacc74236207fbb5e
Perdyx <29049651+Perdyx@users.noreply.github.com>
2021-03-01 15:01:55 -0800

    Update b0x.sh

--- b0x.sh
+++ b0x.sh
@@ -16,24 +16,20 @@ echo " | |_) | |_| |>  < "
 echo " |_.__/ \___//_/\_\\"
 echo -e "\nv2.0.0 - https://github.com/Perdyx/scripts/blob/master/b0x.sh\n"
 
+cd
+
 apt update -y
 apt full-upgrade -y
 
-apt install -y vim vim-airline htop neofetch nmap metasploit-framework build-essential gcc binutils radare2 gcc-multilib file
+apt install -y vim vim-airline htop neofetch nmap metasploit-framework build-essential gcc binutils radare2 gcc-multilib file python3 python3-pip
 
-apt install -y python3-pip
-git clone https://github.com/SusmithKrishnan/torghost.git $HOME/torghost
-cd $HOME/torghost
-chmod +x build.sh
-sh build.sh
-cd
-rm -rf $HOME/torghost
+apt install -y zsh
+chsh -s $(which zsh)
 
 apt install -y gdb
-git clone https://github.com/longld/peda.git ~/peda
-echo "source ~/peda/peda.py" >> ~/.gdbinit
+git clone https://github.com/longld/peda.git $HOME/peda
+echo "source ~/peda/peda.py" >> $HOME/.gdbinit
 
-apt install python3 python3-pip
 pip install pwntools
 
 echo -e "\n\n💀💀💀 Done. Don't forget your configs! 💀💀💀"
